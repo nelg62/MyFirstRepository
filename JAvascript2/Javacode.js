3dc0280d8ae831e8bca4b70696ef6421fe953aa0
nelg62 <glen.harding.nz@gmail.com>
2024-02-19 19:23:54 +1300

    secound commit

---
+++ JAvascript2/Javacode.js
@@ -0,0 +1,29 @@
+function addFun(a, b) {
+  console.log(a + b);
+}
+
+function minusFun(a, b) {
+  console.log(a - b);
+}
+
+function divideFun(a, b) {
+  console.log(a / b);
+}
+
+function timesFun(a, b) {
+  console.log(a * b);
+}
+
+function personName(name) {
+  console.log(`Hello ${name}`);
+}
+
+addFun(5, 5);
+
+minusFun(5, 5);
+
+divideFun(5, 5);
+
+timesFun(5, 5);
+
+personName("Glen");
