509ead7f66f42c591651461368787beb29b229f6
nelg62 <glen.harding.nz@gmail.com>
2024-02-21 18:04:50 +1300

    index js test

--- JAvascript2/Javacode.js
+++ JAvascript2/Javacode.js
@@ -18,6 +18,22 @@ function personName(name) {
   console.log(`Hello ${name}`);
 }
 
+const myArray = ['apple', 'car', 'tree', 'pig', 'watermelon']
+myArray.splice(0, 1, 'monkey')
+myArray.splice(3,1, 'Porcupine')
+myArray.unshift('cabbage')
+myArray.pop()
+
+let object1 = myArray[0]
+let object2 = myArray[3]
+
+myArray[0] = object2
+
+myArray[3] = object1
+
+
+console.log(myArray)
+
 addFun(5, 5);
 
 minusFun(5, 5);
@@ -27,3 +43,11 @@ divideFun(5, 5);
 timesFun(5, 5);
 
 personName("Glen");
+
+console.debug("debug message");
+
+console.info("info message");
+
+console.warn("warning message");
+
+console.error("error message");
